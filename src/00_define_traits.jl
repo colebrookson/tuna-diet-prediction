f7f07a62706754b829ae5b8a913c8bc895ef372c
colebrookson <cole.brookson@gmail.com>
2021-12-17 14:07:36 -0500

    small changes

--- src/00_define_traits.jl
+++ src/00_define_traits.jl
@@ -11,3 +11,9 @@
 # set up =======================================================================
 using DrWatson
 @quickactivate "tuna-diet-prediction"
+
+# define some set of 7 distributions to draw from for the continuous traits
+import Pkg
+Pkg.add(["StatsPlots", "Distributions"])
+using StatsPlots, Distributions
+plot(Geometric(0.6))
