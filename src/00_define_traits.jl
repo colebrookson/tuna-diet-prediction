a442c84c3bcf53cd9ebb49a03b01c4c1a6a20861
colebrookson <cole.brookson@gmail.com>
2021-12-18 16:10:10 -0500

    trait array function

--- src/00_define_traits.jl
+++ src/00_define_traits.jl
@@ -35,3 +35,26 @@ for i in 1:size(M, 1)
     M[i,10] = sample([1,2,3,4,5], Weights([1,1,1,10,1]))
 end
 density(M[:,:])
+
+
+"""
+    draw_sp()
+Generate values for all 10 traits for some given possible species and return 
+as an array.
+"""
+function draw_sp()
+
+    trait_array = zeros(10)
+    trait_array[1] = rand(TruncatedNormal(0.5, 0.15, 0, 1))
+    trait_array[2] = rand(TruncatedNormal(0.2, 0.4, 0, 1))
+    trait_array[3] = rand(TruncatedNormal(0.7, 0.09, 0, 1))
+    trait_array[4] = rand(Beta(5,2))
+    trait_array[5] = rand(Beta(1,4))
+    trait_array[6] = rand(Exponential(0.34))
+    trait_array[7] = rand(truncated(Exponential(0.6), 0, 1))
+    trait_array[8] = sample([1,2,3,4,5], Weights([5,5,1,1,3]))
+    trait_array[9] = sample([1,2], Weights([1,1]))
+    trait_array[10] = sample([1,2,3,4,5], Weights([1,1,1,10,1]))
+
+    return(trait_array)
+end 
