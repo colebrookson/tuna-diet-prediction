a796e457e9af567e0c8e57fbe27f068e7e94a330
colebrookson <cole.brookson@gmail.com>
2021-12-29 01:12:35 -0500

    make prey matrix

--- scripts/00_prey_traits.jl
+++ scripts/00_prey_traits.jl
@@ -1,2 +1,25 @@
+########## 
+##########
+# Set up the prey items and their traits 
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2021-12-28
+##########
+##########
+
+# set up =======================================================================
+
 using DrWatson
 @quickactivate "tuna-diet-prediction"
+include(srcdir("00_define_traits.jl"))
+
+# get all initial species traits by looping through number of prey (50) ========
+
+# set empty matrix
+prey_matrix = zeros(50, 11)
+# fill matrix
+for i in 1:size(prey_matrix, 1)
+    prey_matrix[i, 1] = i 
+    prey_matrix[i, 2:end] = draw_sp()
+end 
