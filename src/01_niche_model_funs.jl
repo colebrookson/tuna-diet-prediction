5d0d5774744dd0a30a66c9a0474f3632bf377a82
colebrookson <cole.brookson@gmail.com>
2022-01-08 12:35:57 -0500

    niche position function

--- src/01_niche_model_funs.jl
+++ src/01_niche_model_funs.jl
@@ -18,6 +18,30 @@ using Distributions, Random, StatsBase, DelimitedFiles, Plots
 
 # specify niche model functions ================================================
 
+"""
+    niche_position(d, j, prey_matrix)
+
+Take in the dimension (trait number 1-10) and the prey identifier, and return 
+the trait value for that prey item (i.e. it's niche position)
+"""
+function niche_position(
+    d::int, # the dimension of the trait 
+    j::int, # prey species 
+    prey_matrix::Array # the array of all prey traits 
+    )
+
+    return(prey_matrix[j,d+1])
+
+end 
+
+
 function niche_model(
     alpha::
 )
+
+
+# tests 
+C_num = readdlm(datadir("./trait_data/predator-numeric-traits.csv"), ',', Any)
+C_cat = readdlm(datadir("./trait_data/predator-categorical-traits.csv"), ',', Any)
+prey_matrix = readdlm(datadir("./trait_data/prey-matrix.csv"), ',', Any)
+prey_abund = fill(1.0, 50)
