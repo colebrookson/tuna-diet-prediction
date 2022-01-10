4b1a972cd1278320465aeec03d1719ea888385e5
colebrookson <cole.brookson@gmail.com>
2022-01-10 00:29:58 -0700

    turing stuff

--- scripts/03_niche_model.jl
+++ scripts/03_niche_model.jl
@@ -14,9 +14,22 @@ include("./00_setup.jl")
 
 using DrWatson
 @quickactivate "tuna-diet-prediction"
-using Distributions, Random, StatsBase, DelimitedFiles, Plots
+using Distributions, Random, StatsBase, DelimitedFiles, Plots, Turing
 
 prey_interactions = readdlm(datadir("./trait_data/interaction-strengths.csv"), ',', Any)
 
 # specify niche model functions ================================================
+
+@model consumption_regression(x, y, n, σ) = begin 
+    intercept ~ Normal(0,σ)
+    α ~ Normal(0,σ)
+    n ~ Normal(0,σ)
+    c ~ Normal(0,σ)
+    r ~ Normal(0,σ)
+
+    for i = 1:n
+        prob = alpha*exp(-((n-c)/(r/2))^2)
+        y[i] ~ Bernoulli(prob)
+    end 
+end;
 
